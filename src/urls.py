9c4a26f5f3fd922292ff051429cc420f0f6c0e3b
Daker Fernandes Pinheiro <daker.pinheiro@openbossa.org>
2011-11-17 23:15:17 -0300

    Add static file for heroku to serve
    Signed-off-by: Daker Fernandes Pinheiro <daker.pinheiro@openbossa.org>

--- src/urls.py
+++ src/urls.py
@@ -1,3 +1,4 @@
+from django.conf import settings
 from django.conf.urls.defaults import patterns, include, url
 from django.contrib import admin
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
@@ -12,6 +13,7 @@ urlpatterns = patterns('',
     url(r'^contact$', core.contact, name='contact'),
     url(r'^blog/', include('blog.urls')),
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^static/(?P<path>.*)$', 'django.contrib.staticfiles.views.serve', {'document_root': settings.STATIC_ROOT, 'insecure': True})
 )
 
 urlpatterns += staticfiles_urlpatterns()
